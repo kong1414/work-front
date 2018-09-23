ba55b4bdc686c61d5af3aa287efba0ba19ee6b0a
qiuguanlin <578482558@qq.com>
2018-09-23 13:21:46 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    <h1>hello world</h1>
     <router-view/>
   </div>
 </template>
---
+++ src/views/Login.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="login-page">
+    
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'login',
+  mounted: {
+
+  }
+}
+</script>
+
+<style lang="sass">
+
+</style>
+
